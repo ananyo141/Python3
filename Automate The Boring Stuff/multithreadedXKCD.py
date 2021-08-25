fca16d2d9cdda7365f7c9de91f9229c3b0efdc6d
ananyo141 <ananyo141@gmail.com>
2021-08-25 10:07:37 +0530

    working state downloadXkcd function

--- Automate The Boring Stuff/multithreadedXKCD.py
+++ Automate The Boring Stuff/multithreadedXKCD.py
@@ -1,18 +1,18 @@
 # Download XKCD comics using multithreading
-import threading, logging, sys, os
+import tkinter.filedialog, threading, logging, os, sys
 from ModuleImporter import module_importer
 requests = module_importer('requests', 'requests')
 bs4 = module_importer('bs4', 'beautifulsoup4')
 
-logging.basicConfig(filename = 'multithreadedXKCD.log', level = logging.DEBUG, format = "%(asctime)s - %(levelname)s - %(lineno)d - %(message)s",
+logging.basicConfig(filename = 'multithreadedXKCD.log', level = logging.INFO, format = "%(asctime)s - %(levelname)s - %(lineno)d - %(message)s",
                     datefmt = '%d/%m/%Y - %I:%M:%S %p', filemode = 'w')
 
 # TODO: Add multithreading
 # TODO: Add time logging
 
 def downloadXkcd(startComic, endComic, directory):
-    '''(int, int) -> None
-    Download the a set of comics from startComic to endComic (non-inclusive)
+    '''(int, int, str) -> None
+    Download the a set of comics from startComic to endComic (exclusive)
     and save to given directory'''
 
     headers = {
@@ -20,11 +20,35 @@ def downloadXkcd(startComic, endComic, directory):
     }
 
     for comicNum in range(startComic, endComic):
+        # download the base page
         try:
-            page = requests.get('https://xkcd.com/' + comicNum, headers = headers)
+            page = requests.get('https://xkcd.com/' + str(comicNum), headers = headers)
             page.raise_for_status()
         except Exception as error:
-            sys.exit(str(error))
+            print(str(error))
+            continue
         pageSoup = bs4.BeautifulSoup(page.text, 'lxml')
-        
+        imgTag = pageSoup.select('#comic img')[0]
+        imgUrl = imgTag.get('src')
+        if not imgTag:
+            print('No comic found!')
+            continue
+        # download the image
+        try:
+            image = requests.get('https:' + imgUrl, headers = headers)
+            image.raise_for_status()
+        except Exception as err:
+            print(str(err))
+            continue
+        # write the downloaded file
+        with open(os.path.join(directory, os.path.basename(imgUrl)), 'wb') as imageFile:
+            for chunk in image.iter_content(1000000):
+                imageFile.write(chunk)
 
+def main():
+    directory = tkinter.filedialog.askdirectory(title = 'Select download directory')
+    if not directory:
+        sys.exit('Download directory not chosen')
+    directory = os.path.normpath(directory)
+
+    
