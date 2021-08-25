2b9327d4de07929c149eb76de1110ed9cb909e89
ananyo141 <ananyo141@gmail.com>
2021-08-25 11:20:35 +0530

    added threading

--- Automate The Boring Stuff/multithreadedXKCD.py
+++ Automate The Boring Stuff/multithreadedXKCD.py
@@ -10,29 +10,30 @@ logging.basicConfig(filename = 'multithreadedXKCD.log', level = logging.INFO, fo
 # TODO: Add multithreading
 # TODO: Add time logging
 
+# This is a constant value that should not change to avoid concurrency issues with threading
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
+}
+
 def downloadXkcd(startComic, endComic, directory):
     '''(int, int, str) -> None
     Download the a set of comics from startComic to endComic (exclusive)
     and save to given directory'''
 
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
-    }
-
     for comicNum in range(startComic, endComic):
         # download the base page
         try:
-            page = requests.get('https://xkcd.com/' + str(comicNum), headers = headers)
+            page = requests.get('https://xkcd.com/' + str(comicNum), headers = headers);        logging.info(f'{page.status_code = }')
             page.raise_for_status()
         except Exception as error:
             print(str(error))
             continue
-        pageSoup = bs4.BeautifulSoup(page.text, 'lxml')
-        imgTag = pageSoup.select('#comic img')[0]
-        imgUrl = imgTag.get('src')
-        if not imgTag:
+        pageSoup = bs4.BeautifulSoup(page.text, 'lxml');                                        logging.debug(f'{pageSoup = }')
+        imgTags = pageSoup.select('#comic img');                                                logging.info(f'{imgTags = }')
+        if not imgTags:
             print('No comic found!')
             continue
+        imgUrl = imgTags[0].get('src');                                                         logging.info(f'{imgUrl = }')                      
         # download the image
         try:
             image = requests.get('https:' + imgUrl, headers = headers)
@@ -41,7 +42,8 @@ def downloadXkcd(startComic, endComic, directory):
             print(str(err))
             continue
         # write the downloaded file
-        with open(os.path.join(directory, os.path.basename(imgUrl)), 'wb') as imageFile:
+        saveName = os.path.join(directory, os.path.basename(imgUrl));                           logging.critical(f'{saveName = }')
+        with open(saveName, 'wb') as imageFile:
             for chunk in image.iter_content(1000000):
                 imageFile.write(chunk)
 
@@ -49,6 +51,42 @@ def main():
     directory = tkinter.filedialog.askdirectory(title = 'Select download directory')
     if not directory:
         sys.exit('Download directory not chosen')
-    directory = os.path.normpath(directory)
-
+    directory = os.path.normpath(directory);                                                    logging.critical(f'{directory = }')
     
+    # note the latest comic
+    try:
+        mainPage = requests.get('https://xkcd.com', headers = headers);                         logging.critical(f'{mainPage.status_code = }')
+        mainPage.raise_for_status()
+    except Exception as error:
+        sys.exit(str(error))
+
+    mainPageSoup = bs4.BeautifulSoup(mainPage.text, 'lxml');                                    logging.debug(f'{mainPageSoup = }')
+    latestComicTag = mainPageSoup.select('#middleContainer > a:nth-child(6)');                  logging.info(f'{latestComicTag = }')
+    try:
+        latestComicLink = latestComicTag[0].get('href');                                        logging.info(f'{latestComicLink = }')
+        latestComicNumber = int(latestComicLink[latestComicLink.rfind('/') + 1 : ]);            logging.info(f'{latestComicNumber = }')
+    except IndexError:  # for latestComicLink
+        sys.exit("Couldn't find the latest comic link")
+    except ValueError:  # for latestComicNumber
+        sys.exit('Unable to parse latest comic number')
+
+    # initialize threading
+    threads = []
+    for start in range(latestComicNumber, 0, -10):
+        end = start - 10
+        if end < 1:
+            end = 1
+        
+        downloadThread = threading.Thread(target = downloadXkcd, args = [end, start, directory])
+        threads.append(downloadThread)
+        downloadThread.start()      # start the thread
+    
+    # pause until all the threads finish
+    for thread in threads:
+        thread.join()
+
+    print('Finished downloading all the comics!')
+
+
+if __name__ == '__main__':
+    main()
