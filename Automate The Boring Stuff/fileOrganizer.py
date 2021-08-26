d4b0a56531d487a8b921df57dab667c02bc4ed17
ananyo141 <ananyo141@gmail.com>
2021-08-27 01:58:10 +0530

    add multithreading, now catch exceptions raised by permission error by send2trash

--- Automate The Boring Stuff/fileOrganizer.py
+++ Automate The Boring Stuff/fileOrganizer.py
@@ -1,7 +1,6 @@
 #!python3
 # This program copies or moves (arranges) files in a folder according to their file extensions.
-
-import tkinter.filedialog, shutil, os
+import tkinter.filedialog, threading, shutil, os, sys
 from pathlib import Path
 from ModuleImporter import module_importer
 pyip = module_importer('pyinputplus', 'pyinputplus')
@@ -16,6 +15,27 @@ def inputDir():
         if chosenDir:
             return os.path.normpath(chosenDir)
 
+def organizer(extension, userDir, destinationDir):
+    extensionDir = os.path.join(destinationDir, extension.lstrip('.').upper())
+    os.makedirs(extensionDir, exist_ok = True)
+    for directory, subdir, filenames in os.walk(userDir):
+        for file in Path(directory).glob('*'+extension): 
+            # if the file already exists               
+            if (Path(extensionDir) / file.name).exists():
+                copyfileNumber = 1
+                # find the latest copy index
+                while True:
+                    newFileName = extensionDir + os.sep + file.stem + '_copy' + \
+                    str(copyfileNumber) + file.suffix
+                    if not os.path.exists(newFileName):
+                        break
+                    copyfileNumber += 1
+                
+                # rename the file with the new copy number
+                shutil.copy(file, newFileName)
+            else:
+                shutil.copy(file, extensionDir)
+
 def main():
     print("Choose the directory to clean and organize")
     userDir = inputDir()
@@ -25,7 +45,7 @@ def main():
     if choice == 'Rearrange the files at the current directory':
         rearrange = True
         destinationDir = userDir + '_arranged'
-        os.mkdir(destinationDir)
+        os.makedirs(destinationDir, exist_ok = True)
     else:
         rearrange = False
         print("Choose the folder to save arranged files")
@@ -41,30 +61,26 @@ def main():
     fileExtensions = list(set(fileExtensions))
 
     # Crawl the directory again to copy these files according to their extension in a new directory
+    threads = []    # keeping threads
     for extension in fileExtensions:
-        extensionDir = os.path.join(destinationDir, extension.lstrip('.').upper())
-        os.mkdir(extensionDir)
-        for directory, subdir, filenames in os.walk(userDir):
-            for file in Path(directory).glob('*'+extension): 
-                # if the file already exists               
-                if (Path(extensionDir) / file.name).exists():
-                    copyfileNumber = 1
-                    # find the latest copy index
-                    while True:
-                        newFileName = extensionDir + os.sep + file.stem + '_copy' + str(copyfileNumber) + file.suffix
-                        if not os.path.exists(newFileName):
-                            break
-                        copyfileNumber += 1
-                    
-                    # rename the file with the new copy number
-                    shutil.copy(file, newFileName)
-                else:
-                    shutil.copy(file, extensionDir)
-
+        organizingThread = threading.Thread(target = organizer, 
+                                            args = [extension, userDir, destinationDir])
+        threads.append(organizingThread)
+        organizingThread.start()
+    
+    # wait for all thread operations to finish
+    for thread in threads:
+        thread.join()
+        
     if rearrange:
         deletePrevFileSystem = pyip.inputYesNo(prompt="Do you want to delete the previous folder?: ")
         if deletePrevFileSystem == 'yes':
-            send2trash.send2trash(userDir)
+            try:
+                send2trash.send2trash(userDir)
+            except Exception as err:        # in case permission is denied by os
+                print("Permission Denied\n" + str(err) + '\n')
+                sys.exit(f"You can find the organised files at {destinationDir}")
+
             os.rename(destinationDir, userDir)
             print("Previous filesystem was organized successfully")
         else:
