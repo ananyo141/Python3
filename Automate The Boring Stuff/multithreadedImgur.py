ff6654b14f9a374489d13c1016f2dac6075a4acf
ananyo141 <ananyo141@gmail.com>
2021-08-26 13:30:07 +0530

    add multithreading, add filename normalization for error free saving

--- Automate The Boring Stuff/multithreadedImgur.py
+++ Automate The Boring Stuff/multithreadedImgur.py
@@ -1,13 +1,46 @@
 #!python3
 # This program takes user input from command line and downloads images into user selected folder
 
-import requests, bs4, sys, os, tkinter.filedialog, time, logging
+import threading, sys, os, tkinter.filedialog, time, logging
+from ModuleImporter import module_importer
+requests = module_importer('requests', 'requests')
+bs4 = module_importer('bs4', 'beautifulsoup4')
+slugify = module_importer('slugify', 'python-slugify')
 
-logging.basicConfig(filename='multhreadedImgur.log', level=logging.INFO, format="%(asctime)s - %(levelname)s - %(lineno)d - %(message)s",
-                    datefmt='%d/%m/%Y - %I:%M:%S %p', filemode='w')
+logging.basicConfig(filename = 'multhreadedImgur.log', level = logging.INFO, format = "%(asctime)s - %(levelname)s - %(lineno)d - %(message)s",
+                    datefmt = '%d/%m/%Y - %I:%M:%S %p', filemode = 'w')
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36',
 }
+
+def downloadImgur(mainPageSoup, i, imagesLinks, downloadDir):
+    imageName = mainPageSoup.select('div.hover>p')[i].getText().replace('/', ' ')[:100];    logging.warning(f'{imageName = }')
+    imageUrl = imagesLinks[i].get('href');                                                  logging.info(f'{imageUrl = }')
+    imageCode = imageUrl[imageUrl.rfind('/'):];                                             logging.warning(f'{imageCode = }')
+
+    print(f"Downloading Image {i + 1}: {imageName[:50]}...")
+    try:
+        downloadImage = requests.get('https://i.imgur.com' + imageCode + '.jpg', headers = headers)
+        downloadImage.raise_for_status()
+    except Exception as exc:
+        print("\nUnexpected error:\n" + str(exc))
+        return
+
+
+        
+    # writing the downloaded file
+    fileName = downloadDir + os.sep + slugify.slugify(imageName) + '.jpg';                  logging.critical(f'{fileName = }')
+    # for duplicate image names
+    count = 1
+    fileStem = os.path.splitext(fileName)[0];                                               logging.critical(f'{fileStem = }')
+    while os.path.exists(fileName):
+        fileName = fileStem + str(count) + '.jpg'
+        count += 1
+
+    with open(fileName, mode = 'wb') as file:
+        for chunk in downloadImage.iter_content(1000000):
+            file.write(chunk)
+    # time.sleep(0.4)         # To avoid blocking of script requests
 
 def main():
     if len(sys.argv) < 2:
@@ -22,79 +55,38 @@ def main():
     # Take the download directory
     downloadDir = tkinter.filedialog.askdirectory()
     if not downloadDir:
-        sys.exit("No directory chosen.")
-    logging.debug(f'{downloadDir = }')
+        sys.exit("No directory chosen.");                                           logging.debug(f'{downloadDir = }')
+    downloadDir = os.path.normpath(downloadDir)
 
-    url = 'https://imgur.com/r/' + keywords
-    logging.info(f'{url = }')
+    url = 'https://imgur.com/r/' + keywords;                                        logging.info(f'{url = }')
 
-    tries = 0
-    while True:
-        try:
-            mainPage = requests.get(url, headers = headers)
-            logging.warning(f'{mainPage.status_code = }')
-            mainPage.raise_for_status()
-            if mainPage.status_code == requests.codes.ok:
-                break
-        except Exception as exc:
-            print("\nUnable to connect to Imgur.")
-            if tries > 5:
-                sys.exit('Check your internet connection and try again.\n\n' + str(exc))
-            print(f"Retrying...({tries})")
-            tries += 1
-            time.sleep(1)
-            continue
+    try:
+        mainPage = requests.get(url, headers = headers);                            logging.warning(f'{mainPage.status_code = }')
+        mainPage.raise_for_status()
+    except Exception as exc:
+        print("\nUnable to connect to Imgur.")
+        sys.exit('Check your internet connection and try again.\n\n' + str(exc))
 
-    tries = 0
-    mainPageSoup = bs4.BeautifulSoup(mainPage.text, 'lxml')
-    logging.debug(f'{mainPageSoup = }')
+    mainPageSoup = bs4.BeautifulSoup(mainPage.text, 'lxml');                        logging.debug(f'{mainPageSoup = }')
     
     # get the list of the image tags
-    imagesLinks = mainPageSoup.select('a.image-list-link')
-    logging.debug(f'{imagesLinks = }')
+    imagesLinks = mainPageSoup.select('a.image-list-link');                         logging.debug(f'{imagesLinks = }')
     if len(imagesLinks) == 0:
         sys.exit("No images found on Imgur")
 
+    threads = []
+    start_time = time.time()
     for i in range(min(len(imagesLinks), numImages)):
-       
-        imageName = mainPageSoup.select('div.hover>p')[i].getText().replace('/', ' ')[:240]
-        imageUrl = imagesLinks[i].get('href')
-        imageCode = imageUrl[imageUrl.rfind('/'):]
+       downloadThread = threading.Thread(target = downloadImgur, args = [mainPageSoup, i, imagesLinks, downloadDir])
+       threads.append(downloadThread)
+       downloadThread.start()
 
-        logging.info(f"{imagesLinks[i].get('href')}")
-        logging.info(f'{imageUrl = }')
-        logging.warning(f'{imageName = }')
-        logging.warning(f'{imageCode = }')
-        print(f"Downloading Image {i + 1}: {imageName[:50]}...")
-        try:
-            downloadImage = requests.get('https://i.imgur.com' + imageCode + '.jpg', headers = headers)
-            downloadImage.raise_for_status()
-        except Exception as exc:
-            print("\nUnexpected error:\n" + str(exc))
-            if tries > 5:
-                sys.exit()
-            print(f"Retrying...({tries})")
-            tries += 1
-            continue
+    # wait for threads to complete
+    for thread in threads:
+        thread.join()
 
-        tries = 0
-        # writing the downloaded file
-        fileName = downloadDir + os.sep + imageName + '.jpg'
-        # for duplicate image names
-        count = 1
-        fileStem = os.path.splitext(fileName)[0]
-        while os.path.exists(fileName):
-            fileName = fileStem + str(count) + '.jpg'
-            count += 1
-
-        logging.critical(f'{fileName = }')
-        file = open(fileName, mode = 'wb')
-        for chunk in downloadImage.iter_content(1000000):
-            file.write(chunk)
-        file.close()
-        # time.sleep(0.4)         # To avoid blocking of script requests
-
-    print("Download Completed")
+    end_time = time.time()
+    print("Download Completed, time taken = %.2f seconds" % (end_time - start_time))
     print(f"Files saved at directory: {downloadDir}")
 
 
