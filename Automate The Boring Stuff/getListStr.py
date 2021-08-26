75c8fb8ffb01718042ce89f3f411e842fd737050
ananyo141 <ananyo141@gmail.com>
2021-08-26 23:29:09 +0530

    feat: add period at the end of string, update documentation

--- Automate The Boring Stuff/getListStr.py
+++ Automate The Boring Stuff/getListStr.py
@@ -1,19 +1,19 @@
 # WAP that takes a list value as an argument and returns a string with all the items separated by 
 # a comma and a space, with 'and' inserted before the last item.
 
-def getListStr(list):
-    '''(list)-->str
-    Return a string with all the items in the list separated by a comma and a space,
+def getListStr(list: list) -> str:
+    '''Return a string with all the items in the list separated by a comma and a space,
     with 'and' inserted before the last item.
-    >>>getListStr(['apples','guava','tofu','cat'])
-    'apples, guava, tofu, and cat'
+
+    >>>getListStr(['', '', 'apples', '','guava','tofu','cat', ''])
+    'apples, guava, tofu and cat.'
     '''
                                     ### Note To Debugger ###
     # Removing empty strings in list gave undefined behaviour, empty strings were not stored 
     # with proper indexes, duplicate indexes were found. Maybe this is how python stores empty strings in lists.
     # That's why traditional methods aren't working, had to modify the input delivery system to prohibit
     # empty strings in the first place. 
-                                    ### DELETED PREVIOUS OBSOLETE CODE ###
+                                ### DELETED PREVIOUS OBSOLETE CODE ###
 
     # build a list that is devoid of empty strings
     nonEmptyList = []
@@ -28,6 +28,8 @@ def getListStr(list):
             nonEmptyStr += ', '
         elif i < len(nonEmptyList) - 1:     # this is only checked if the first one fails
             nonEmptyStr += ' and '
+        elif i < len(nonEmptyList):
+            nonEmptyStr += '.'
     
     return nonEmptyStr
 
@@ -36,7 +38,7 @@ def main():
     stringList = []
     while True:
         string = input("Enter the strings('done' to exit): ")
-        if string.lower().startswith('done'):
+        if string.lower() == 'done':
             break
         stringList.append(string)
 
