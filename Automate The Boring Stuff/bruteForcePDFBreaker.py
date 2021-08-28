d5e444e11187e4e456f17eb14f5766646304cb5b
ananyo141 <ananyo141@gmail.com>
2021-08-28 12:54:22 +0530

    now start from 0 to len dictionary

--- Automate The Boring Stuff/bruteForcePDFBreaker.py
+++ Automate The Boring Stuff/bruteForcePDFBreaker.py
@@ -11,7 +11,7 @@ def bruteForce(passwords: list, start: int, stop: int, pdfPath: str, maintainer:
         sys.exit('PDF is unencrypted')
     
     # check every word, uppercase and lowercase
-    for i in range(start, stop):
+    for i in range(start, stop):          # stop is non-inclusive
         if maintainer:
             break
         word = passwords[i].strip()
@@ -28,36 +28,41 @@ def bruteForce(passwords: list, start: int, stop: int, pdfPath: str, maintainer:
 def main():
     # Input the dictionary file
     try:
-        possiblePass = tkinter.filedialog.askopenfile(title = 'Input dictionary file', filetypes = [('Text Files','*.txt')]).readlines()
+        possiblePass = tkinter.filedialog.askopenfile(title = 'Input dictionary file',
+                        filetypes = [('Text Files','*.txt')]).readlines()
     except AttributeError:
         sys.exit('Possible list of passwords not given')
     # Input the encrypted file
-    encryptedFile = tkinter.filedialog.askopenfilename(title = 'Input encrypted file', filetypes = [('PDF Files','*.pdf')])
+    encryptedFile = tkinter.filedialog.askopenfilename(title = 'Input encrypted file',
+                        filetypes = [('PDF Files','*.pdf')])
     if not encryptedFile:
         sys.exit('Encrypted pdf not specified')
     
     print('Commencing Brute Force Attack')
 
     processes = []
-    manager = multiprocessing.Manager()
-    maintainer = manager.dict()
-    start_time = time.time()
-    # take start matching from end to tackle non-multiple indexes of (len(possiblePass) / 12)
-    for end in range(len(possiblePass), 0, -(int(len(possiblePass) / 12))):
-        start = end - (int(len(possiblePass) / 12));          
-        if start < 0:
-            start = 0
+    manager = multiprocessing.Manager()   # processes manager (shared variable: dict) that,
+    maintainer = manager.dict()           # communicates status between the processes.
+    start_time = time.time()              # mark start time
+    # Create 12 processes, each tackling len(possiblePass) / 12 passwords
+    NUM_PROCESSES = 12                    # constant processess to be executed
+    passwordsPerProcess = int(len(possiblePass) / NUM_PROCESSES)
+
+    for start in range(0, len(possiblePass) + 1, passwordsPerProcess):
+        end = start + passwordsPerProcess;          
+        if end > len(possiblePass):       # if end out of bounds,
+            end = len(possiblePass)       # reset to last valid index
 
         process = multiprocessing.Process(target = bruteForce, args = [possiblePass, start, end, encryptedFile, maintainer])
         processes.append(process)
         process.start()
 
-    # join every process in main process
+    # join child processes in main process
     for process in processes:
         process.join()
 
-    end_time = time.time()
-
+    end_time = time.time()                # mark end time
+    
     if maintainer:
         print(f"\nPdf password cracked '{maintainer['Password']}' in {round(end_time - start_time, 2)} seconds")
     else:
