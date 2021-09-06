927a97dbd2491378d302e700d176f30d9b2474a8
ananyo141 <ananyo141@gmail.com>
2021-09-06 10:28:33 +0530

    changed emailer from selenium to smtplib

--- My Little Bugs/emailerSelenium.py
+++ My Little Bugs/emailerSelenium.py
@@ -7,58 +7,60 @@
 ######################################################################################                                                                                      
 
 # Send an email with the help of Selenium
-import tkinter.filedialog, pyinputplus as pyip, datetime, sys
-from selenium import webdriver
+import tkinter.filedialog, smtplib, textwrap, datetime, sys
+from ModuleImporter import module_importer
 
-# TODO: ask for the gecko driver
-# TODO: ask for recipient address
-# TODO: ask for email subject and body (give option to select a text file)
-# TODO: ask for sender email
-# TODO: save draft and delete if send successful
-# TODO: ask for sender password
-# TODO: fill in the information and send the email and log out
+pyip = module_importer('pyinputplus', 'pyinputplus')
 
 def timestamp():
+    ''' Capture the device date and time information and return the formatted string '''
     now = datetime.datetime.now()
-    return f'{now.strftime("%a")} {now.strftime("%b")} ' \
-        f'{now.strftime("%d")}, {now.strftime("%Y")} {now.strftime("%l")}:'\
-        f'{now.strftime("%M")}:{now.strftime("%S")} {now.strftime("%p")}'
+    return now.strftime("%a %b %d, %Y %I:%M:%S %p")
 
 def main():
-    print("Select the gecko driver for Mozilla Firefox:")
-    gecko = tkinter.filedialog.askopenfilename()
-    if not gecko:
-        sys.exit("Gecko driver not selected")
     
+    # ask for recipient address
     recipient = pyip.inputEmail(prompt = "Enter Recipient Email: ")
+    # ask for email subject and body (give option to select a text file)
     subject = input("Enter subject: ")
     choice = pyip.inputMenu(['Enter text by typing', 'Enter from a text file'], prompt = 'Email Body:\n', numbered = True)
     if choice.lower() == 'enter text by typing':
         content = input("Enter the body of email:\n")
     elif choice.lower() == 'enter from a text file':
-        content = tkinter.filedialog.askopenfile(filetypes = [('Text', '.txt')], mode = 'r').read()
+        content = tkinter.filedialog.askopenfile(filetypes = [('Text', '*.txt')], mode = 'r').read()
     
-    sender = pyip.inputEmail(prompt = "Enter Sender email: ")
-
-    # save draft in case of failure
-    draft = open('./draft.txt', mode = 'w')
-    draft.write(f'To: {recipient}\n\n')
-    draft.write(f'Subject: {subject}'.center(100) + '\n\n')
-    draft.write(content + '\n')
-    draft.write(f'From: {sender}'.rjust(100) + '\n')
-    draft.write(timestamp().rjust(100))
-    draft.close()
-
+    # ask for sender email, password
+    sender = pyip.inputEmail(prompt = "Enter your email: ")
     password = pyip.inputPassword(prompt = f'Enter gmail account password for {sender}: ')
 
-    # Send email with Selenium
-    browser = webdriver.Firefox(executable_path = gecko)
-    browser.get('https://accounts.google.com/ServiceLogin?service=mail')
-    userElem = browser.find_element_by_id('identifierId')
-    userElem.send_keys(sender)
-    nextElem = browser.find_element_by_css_selector('.VfPpkd-LgbsSe-OWXEXe-k8QpJ > span:nth-child(3)')
-    nextElem.submit()
+    # initialize the wapper object to write wrapped text
+    wrapper = textwrap.TextWrapper(width = 70)
 
+    # save draft in case of failure
+    with open('./draft.txt', mode = 'w') as draft:
+        draft.write(f'To: {recipient}\n\n')
+        draft.write(f'Subject: {wrapper.fill(text = subject)}'.center(100) + '\n\n')
+        draft.write(wrapper.fill(text = content) + '\n')
+        draft.write(f'From: {sender}'.rjust(100) + '\n')
+        draft.write(timestamp().rjust(100))
+
+    # TODO: fill in the information and send the email and close connection
+    # start connection
+    try:
+        smtpObj = smtplib.SMTP('smtp.gmail.com', 587)
+    except smtplib.socket.gaierror:
+        sys.exit("Unable to connect to gmail smtp serve")
+    
+    smtpObj.ehlo()  # say hello to the server (identify script)
+    smtpObj.starttls() # start encryption mode
+    try:
+        smtpObj.login(sender, password)
+    except smtplib.SMTPAuthenticationError:
+        sys.exit("Invalid email or password provided\n"
+            "Tip: Make sure you have unsecured app access enabled in your Gmail settings")
+
+    smtpObj.sendmail(sender, recipient, f"{subject}\n{content}")
+    smtpObj.quit()
 
 
 if  __name__ == '__main__':
