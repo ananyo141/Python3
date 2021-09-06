53cec71e92b1d7ed2a7ae50dcee65bba7f8c5030
ananyo141 <ananyo141@gmail.com>
2021-09-06 10:56:56 +0530

    resolve bug, deindent to send email per person

--- Automate The Boring Stuff/emailDues.py
+++ Automate The Boring Stuff/emailDues.py
@@ -48,35 +48,36 @@ def main():
 
     # Find the members who have not paid their dues
     for rowNum in range(2, xlSheet.max_row + 1): # first row contains headings and month names
+        monthsDue = []  # keep track of due months
         for colNum in range(3, xlSheet.max_column + 1): # first two columns contain name and email details
-            monthsDue = []
             if xlSheet.cell(row = rowNum, column = colNum).value != 'paid':
                 defaultedMonth = xlSheet.cell(row = 1, column = colNum).value
                 monthsDue.append(defaultedMonth)
-            if not monthsDue:
-                continue    # if no months were due
 
-            # send the personalized email
-            defaulterName = xlSheet.cell(row = rowNum, column = 1).value
-            recipientEmail = xlSheet.cell(row = rowNum, column = 2).value
+        if not monthsDue:
+            continue    # if no months were due
 
-            message = (f"Hey {defaulterName}, this is to inform you that " 
-                        f"we still haven't got your payment for the month of "
-                        f"{', '.join(monthsDue)} and since we are on a tight budget, "
-                        "it will be super helpful if you can settle the due within "
-                        "the end of this month. Looking forward to meet you!\n\nAni.")
-            try:
-                status = sendEmail(senderEmail, recipientEmail,
-                            password = password, subject = "Fees Due", content = message)
-            except smtplib.socket.gaierror:
-                sys.exit("Unable to connect to gmail smpt server")
-            except smtplib.SMTPAuthenticationError:
-                sys.exit(f"Invalid email or password provided\n"
-                    "Tip: Make sure you have 'unsecured app access' enabled in your Gmail settings")
-            if status != {}:
-                print("Error sending email")
-            else:
-                print(f"Email sent to {recipientEmail} for due month {', '.join(monthsDue)}...")                
+        # send the personalized email
+        defaulterName = xlSheet.cell(row = rowNum, column = 1).value
+        recipientEmail = xlSheet.cell(row = rowNum, column = 2).value
+
+        message = (f"Hey {defaulterName}, this is to inform you that " 
+                    f"we still haven't got your payment for the month of "
+                    f"{', '.join(monthsDue)} and since we are on a tight budget, "
+                    "it will be super helpful if you can settle the due within "
+                    "the end of this month. Looking forward to meet you!\n\nAni.")
+        try:
+            status = sendEmail(senderEmail, recipientEmail,
+                        password = password, subject = "Fees Due", content = message)
+        except smtplib.socket.gaierror:
+            sys.exit("Unable to connect to gmail smpt server")
+        except smtplib.SMTPAuthenticationError:
+            sys.exit(f"Invalid email or password provided\n"
+                "Tip: Make sure you have 'unsecured app access' enabled in your Gmail settings")
+        if status != {}:
+            print("Error sending email")
+        else:
+            print(f"Email sent to {recipientEmail} for due month {', '.join(monthsDue)}...")                
 
     print("Emails were sent to the defaulters")
 
