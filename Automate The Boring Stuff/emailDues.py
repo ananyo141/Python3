dec25a04fadd3c8f8c459ab1e73a3c1e95934c58
ananyo141 <ananyo141@gmail.com>
2021-09-06 10:42:50 +0530

    send email per person not month, provide password uphand

--- Automate The Boring Stuff/emailDues.py
+++ Automate The Boring Stuff/emailDues.py
@@ -41,6 +41,7 @@ def sendEmail(sender, recipient, **kwargs):
 def main():
     membershipFile = tkinter.filedialog.askopenfilename(filetypes = [('Excel Spreadsheet', '*.xlsx')])
     senderEmail = pyip.inputEmail("Enter sender email: ")
+    password = pyip.inputPassword(f"Enter password for {senderEmail}: ")
     
     xlFile = openpyxl.load_workbook(membershipFile)
     xlSheet = xlFile.active
@@ -48,30 +49,36 @@ def main():
     # Find the members who have not paid their dues
     for rowNum in range(2, xlSheet.max_row + 1): # first row contains headings and month names
         for colNum in range(3, xlSheet.max_column + 1): # first two columns contain name and email details
+            monthsDue = []
             if xlSheet.cell(row = rowNum, column = colNum).value != 'paid':
-                # send the personalized email
-                defaulterName = xlSheet.cell(row = rowNum, column = 1).value
                 defaultedMonth = xlSheet.cell(row = 1, column = colNum).value
-                recipientEmail = xlSheet.cell(row = rowNum, column = 2).value
+                monthsDue.append(defaultedMonth)
+            if not monthsDue:
+                continue    # if no months were due
 
-                message = (f"Hey {defaulterName}, this is to inform you that" 
-                           f"we still haven't got your payment for the month of"
-                           f"{defaultedMonth} and since we are on a tight budget,"
-                           "it will be super helpful if you can settle the due within"
-                           "the end of this month. Looking forward to meet you!\n\nAni.")
-                try:
-                    status = sendEmail(senderEmail, recipientEmail, subject = "Fees Due", content = message)
-                except smtplib.socket.gaierror:
-                    sys.exit("Unable to connect to gmail smpt server")
-                except smtplib.SMTPAuthenticationError:
-                    sys.exit(f"Invalid email or password provided\n"
-                        "Tip: Make sure you have 'unsecured app access' enabled in your Gmail settings")
-                if status != {}:
-                    print("Error sending email")
-                else:
-                    print(f"Email sent to {recipientEmail} for due month {defaultedMonth}...")                
+            # send the personalized email
+            defaulterName = xlSheet.cell(row = rowNum, column = 1).value
+            recipientEmail = xlSheet.cell(row = rowNum, column = 2).value
 
+            message = (f"Hey {defaulterName}, this is to inform you that " 
+                        f"we still haven't got your payment for the month of "
+                        f"{', '.join(monthsDue)} and since we are on a tight budget, "
+                        "it will be super helpful if you can settle the due within "
+                        "the end of this month. Looking forward to meet you!\n\nAni.")
+            try:
+                status = sendEmail(senderEmail, recipientEmail,
+                            password = password, subject = "Fees Due", content = message)
+            except smtplib.socket.gaierror:
+                sys.exit("Unable to connect to gmail smpt server")
+            except smtplib.SMTPAuthenticationError:
+                sys.exit(f"Invalid email or password provided\n"
+                    "Tip: Make sure you have 'unsecured app access' enabled in your Gmail settings")
+            if status != {}:
+                print("Error sending email")
+            else:
+                print(f"Email sent to {recipientEmail} for due month {', '.join(monthsDue)}...")                
 
+    print("Emails were sent to the defaulters")
 
 
 if __name__ == '__main__':
