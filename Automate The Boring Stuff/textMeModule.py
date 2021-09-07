bbe3a31e315fdf64535c774cbe4ba1c711163994
ananyo141 <ananyo141@gmail.com>
2021-09-07 14:09:27 +0530

    added checking for variables if set

--- Automate The Boring Stuff/textMeModule.py
+++ Automate The Boring Stuff/textMeModule.py
@@ -9,9 +9,16 @@ def textTo(number: str, message: str) -> None:
     authorization tokens in the environment variables '''
 
     # get preset values from the os environment variables with these names:
-    accountSID = os.environ.get('accountSID')
-    authToken = os.environ.get('authToken')
-    twilioNumber = os.environ.get('twilioNumber')
+    accountSID = os.environ.get('accountSID')   # twilio account SID
+    authToken = os.environ.get('authToken')  # twilio authorizaton token
+    twilioNumber = os.environ.get('twilioNumber') # number provided by twilio to send text
+
+    if not accountSID:
+        raise Exception("Environment variable for accountSID not set")
+    if not authToken:
+        raise Exception("Environment variable for authToken not set")
+    if not twilioNumber:
+        raise Exception("Environment variable for twilioNumber not set")
 
     twilioCli = Client(accountSID, authToken)
     twilioCli.messages.create(body = message, from_ = twilioNumber, to = number)
