319dccc807b7d07ff7b419da5ca1289cb9a50f01
ananyo141 <ananyo141@gmail.com>
2021-09-12 14:46:54 +0530

    print program execution

--- Automate The Boring Stuff/photoInvitation.py
+++ Automate The Boring Stuff/photoInvitation.py
@@ -1,6 +1,10 @@
 # Create custom invitation for guests in the guests.txt file with background image
 # specified by the user from the internet
-import tkinter.filedialog, requests, sys, os
+import tkinter.filedialog, sys, os
+from ModuleImporter import module_importer
+
+PIL = module_importer('PIL', 'Pillow')
+requests = module_importer('requests', 'requests')
 from PIL import Image, ImageDraw, ImageFont
 
 def main():
@@ -8,15 +12,19 @@ def main():
                filetypes = [('Text File', '*.txt')])
     if not textFile:
         sys.exit("No file chosen")
+
     imageLink = input("Enter the link of image: ")
     if not imageLink:
         sys.exit("No image link given")
+
     saveDir = os.path.join(os.path.dirname(textFile), 'Invitations')
-    os.makedirs(saveDir, exist_ok=True)
+    os.makedirs(saveDir, exist_ok = True)
     # Open the guests text file and read contents
+    print("Reading text file...")
     guests = open(textFile).read().split('\n')
 
     # Download the file, keep it as temporary file
+    print("Downloading image...")
     try:
         image = requests.get(imageLink)
         image.raise_for_status()
@@ -31,7 +39,8 @@ def main():
     with Image.open('_temp.png') as template:
         template = template.resize((600, 300))
         for guest in guests:
-        # For every guest, copy a template and write the name of the guest
+            # For every guest, copy a template and write the name of the guest
+            print("Creating invitation for %s" % guest)
             baseImage = template.copy()
             monotypeFont = ImageFont.truetype('MTCORSVA.TTF', 35)
             draw = ImageDraw.Draw(baseImage)
@@ -39,8 +48,9 @@ def main():
             # Save the file
             baseImage.save(os.path.join(saveDir, guest + '.png'))
 
+    print("Deleting temporary files...")
     os.unlink('_temp.png')
-
+    print("Invitations created at %s" % saveDir)
 
 if __name__ == '__main__':
     main()
